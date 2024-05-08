adfe09ebc155191c1ed3f03068a48fa4586c0485
3bdalrahman <abdomoslaam@gmail.com>
2024-05-08 23:10:56 +0300

    kotlin

--- app/src/main/java/com/example/wagbat/SignUp.kt
+++ app/src/main/java/com/example/wagbat/SignUp.kt
@@ -1,26 +1,23 @@
 package com.example.wagbat
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+
+class SignUp : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var signup_button :Button
+    private lateinit var email :EditText
+    private lateinit var password :EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        var move=findViewById<Button>(R.id.move)
 
-        move.setOnClickListener {
-
-            var nextpage= Intent(this,SignUp::class.java)
-            startActivity(nextpage)
-            finish()
-        }
-        }
-
+    }
 }
