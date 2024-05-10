7d6e651c5d79db2415e944fc161842e1b97f1f9c
Amr11matarek <110143582+Amr11matarek@users.noreply.github.com>
2024-05-10 17:32:35 +0300

    Navigation bar

--- app/src/main/java/com/example/wagbat/EndOfApp.kt
+++ app/src/main/java/com/example/wagbat/EndOfApp.kt
@@ -1,15 +1,33 @@
 package com.example.wagbat
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EndOfApp : AppCompatActivity() {
+
+
+    lateinit var backToHome: Button
+    lateinit var exit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_end_of_app)
+
+        val backIntent = Intent(this, Home::class.java)
+
+        backToHome = findViewById(R.id.BackToHomeBtn2)
+        exit = findViewById(R.id.ExitBtn)
+
+        backToHome.setOnClickListener {
+            startActivity(backIntent)
+        }
+        exit.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/wagbat/Home.kt
+++ app/src/main/java/com/example/wagbat/Home.kt
@@ -3,24 +3,55 @@ package com.example.wagbat
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import java.util.Locale
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.internal.NavigationMenuPresenter
+import com.google.android.material.navigation.NavigationView
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(){
     lateinit var recycler: RecyclerView
     lateinit var product_list: ArrayList<item>
     lateinit var img_id: Array<Int>
     lateinit var names: Array<String>
     lateinit var search : SearchView
     lateinit var adapter : adapter
+
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationView.OnNavigationItemSelectedListener { menuItem -> onNavigationItemSelected(menuItem) }
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open_nav,R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null){
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,MyProfile()).commit()
+            navigationView.setCheckedItem(R.id.nav_profile)
+
+        }
 
         img_id = arrayOf(
             R.drawable.mac,
@@ -92,6 +123,32 @@ class Home : AppCompatActivity() {
         }
     }
 
+
+    private fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_profile -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,MyProfile()).commit()
+            R.id.nav_orders -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,MyOrders()).commit()
+            R.id.nav_FAQs -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container,FAQs()).commit()
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+        }else{
+
+            onBackPressedDispatcher.onBackPressed()
+
+        }
+    }
 
     private fun getData()
     {
