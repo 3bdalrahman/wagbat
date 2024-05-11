afe9261358449a890f7aa4c085a39b9ad1cb4693
3bdalrahman <abdomoslaam@gmail.com>
2024-05-11 13:37:40 +0300

    on click listeners on dish details

--- app/src/main/java/com/example/wagbat/Dishdetails.kt
+++ app/src/main/java/com/example/wagbat/Dishdetails.kt
@@ -1,20 +1,33 @@
 package com.example.wagbat
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class Dishdetails : AppCompatActivity() {
+
+    private lateinit var incBtn: Button
+    private lateinit var decBtn: Button
+    private lateinit var price: TextView
+    private lateinit var counter: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dishdetails)
-
         val details= intent.getParcelableExtra<dishs>("name")
+        incBtn = findViewById(R.id.inc2Button)
+        decBtn = findViewById(R.id.dec2Button)
+        price = findViewById(R.id.Home_Screen3_Price)
+        counter = findViewById(R.id.inc_dec2)
+
         if(details!=null)
         {
             val name : TextView = findViewById(R.id.Home_Screen3_Name)
@@ -24,6 +37,26 @@ class Dishdetails : AppCompatActivity() {
             name.text = details.dish_name
             price.text = details.price
             img.setImageResource(details.dish_img)
+        }
+        var priceInt = price.text.toString().toInt()
+        var counterInt = 1
+        var FirstPrice = price.text.toString().toInt()
+        incBtn.setOnClickListener {
+            if (counterInt > 0) {  // Check before incrementing
+                counterInt++
+                priceInt += FirstPrice
+            }
+            counter.text = counterInt.toString()
+            price.text = priceInt.toString()
+        }
+
+        decBtn.setOnClickListener {
+            if (counterInt > 1) {  // Check before incrementing
+                counterInt--
+                priceInt -= FirstPrice
+            }
+            counter.text = counterInt.toString()
+            price.text = priceInt.toString()
         }
 
     }
