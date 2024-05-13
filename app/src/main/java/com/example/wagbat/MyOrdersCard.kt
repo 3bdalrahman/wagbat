22bd788cf2f735b18f3b4d275348606ac6c1054b
Amr11matarek <110143582+Amr11matarek@users.noreply.github.com>
2024-05-13 17:28:53 +0300

    History

---
+++ app/src/main/java/com/example/wagbat/MyOrdersCard.kt
@@ -0,0 +1,15 @@
+package com.example.wagbat
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MyOrdersCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_orders_card)
+    }
+}
